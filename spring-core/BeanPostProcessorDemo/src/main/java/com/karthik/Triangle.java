2d7a3b22be7509475dc474429863ce2aa51e35f7
Karthik <chejerlakarthik@github.com>
2018-05-10 16:06:13 +0530

    PostConstruct and PreDestroy JSR-250 annotations

--- spring-core/BeanPostProcessorDemo/src/main/java/com/karthik/Triangle.java
+++ spring-core/BeanPostProcessorDemo/src/main/java/com/karthik/Triangle.java
@@ -1,5 +1,8 @@
 package com.karthik;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class Triangle implements Shape{
 
 	public void draw(int i) {
@@ -8,6 +11,16 @@ public class Triangle implements Shape{
 	
 	public void init() {
 		System.out.println("Created a triangle bean");
+	}
+	
+	@PostConstruct
+	public void postconstruct() {
+		System.out.println("JSR-250 @PostConstruct - Not Coupled with Spring");
+	}
+	
+	@PreDestroy
+	public void predestroy() {
+		System.out.println("JSR-250 @PreDestroy - Not Coupled with Spring");
 	}
 
 }
